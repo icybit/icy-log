8feca72bc7f28d7b445f6d29542bfa39ab5517db
Giedrius Sirevicius <giedriussir@gmail.com>
2015-12-06 19:27:15 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,181 @@
+﻿'use strict';
+
+var async = require('async'),
+    debug = require('debug')('icybit:error-handler');
+
+module.exports = icylog;
+
+function icylog(opts) {
+    var options = opts || {},
+        env = options.env || 'development',
+        logger = options.logger || { error: function () { } };
+
+    debug('Setting up middleware for the ' + env + ' environment');
+
+    return {
+        http: httpHandler,
+        ws: wsHandler
+    };
+
+    function httpHandler(err, req, res, next) {
+        debug('Received error: ' + err);
+        debug('Invoking HTTP handler middleware');
+
+        preHandler(err, function (err, results) {
+            var error, handler, payload;
+
+            if (err) {
+                return next(unexpectedHandler(err));
+            } else {
+                error = results[0];
+                handler = results[1];
+                payload = results[2];
+
+                handler(error, function () {
+                    handleHttpResponse(error, req, res, payload);
+                });
+            }
+        });
+
+        function handleHttpResponse(err, req, res, payload) {
+            debug('Submitting HTTP response');
+
+            res.status(err.status);
+            res.setHeader('X-Content-Type-Options', 'nosniff');
+
+            res.format({
+                json: function () {
+                    res.send(payload);
+                },
+                text: function () {
+                    res.send(JSON.stringify(payload));
+                },
+                'default': function () {
+                    res.status(406).type('text').send('Not Acceptable');
+                }
+            });
+        }
+    }
+
+    function preHandler(error, callback) {
+        var locals = {
+            err: error
+        };
+
+        debug('Attempting to pre-process error');
+
+        async.series([
+            ensureError.bind(null, locals),
+            electHandler.bind(null, locals),
+            createPayload.bind(null, locals)
+        ], callback);
+
+        function createPayload(locals, callback) {
+            var err = locals.err,
+                payload = {
+                    success: false,
+                    message: err.message
+                };
+
+            if (env === 'development') {
+                payload.error = err;
+            }
+
+            return callback(null, payload);
+        }
+
+        function electHandler(locals, callback) {
+            var type = locals.err.status.toString()[0],
+            category = parseInt(type);
+
+            switch (category) {
+                case 4: {
+                    debug('Elected Client handler');
+                    return callback(null, clientHandler);
+                }
+                case 5: {
+                    debug('Elected Server handler');
+                    return callback(null, serverHandler);
+                }
+                default: {
+                    debug('Failed to elect handler');
+                    return callback(new Error('The HTTP Status Code is neither 4xx or 5xx. Refusing to treat as Error'));
+                }
+            };
+
+            function clientHandler(err, callback) {
+                debug('Invoking Client handler');
+                logger.error('Client Error. %s', err.toString());
+                return callback(null);
+            }
+
+            function serverHandler(err, callback) {
+                debug('Invoking Server handler');
+                logger.error('Internal Server Error. %s', err.toString());
+                return callback(null);
+            }
+        }
+
+        function ensureError(locals, callback) {
+            var err = locals.err;
+
+            if (!(err instanceof Error)) {
+                err = new Error(err + '');
+            }
+
+            if (!err.status) {
+                err.status = err.code || err.statusCode || 500;
+            }
+
+            locals.err = err;
+
+            return callback(null, locals.err);
+        }
+    }
+
+    function unexpectedHandler(error) {
+        var payload = {
+            success: false,
+            message: 'An unexpected exception has occurred. ' + error.message
+        };
+
+        if (env === 'development') {
+            payload.error = error;
+        }
+
+        debug('Invoking UnexpectedError handler');
+
+        logger.error('An unexpected exception has occurred. %s', error.toString());
+
+        return payload;
+    }
+
+    function wsHandler(err, callback) {
+        var callback = (typeof callback === 'function' ? callback : function () { });
+
+        debug('Received error: ' + err);
+        debug('Invoking WS handler middleware');
+
+        preHandler(err, function (err, results) {
+            var error, handler, payload;
+
+            if (err) {
+                return callback(unexpectedHandler(err));
+            } else {
+                error = results[0];
+                handler = results[1];
+                payload = results[2];
+
+                handler(error, function () {
+                    handleWsResponse(payload, callback);
+                });
+            }
+        });
+
+        function handleWsResponse(payload, callback) {
+            debug('Submitting WS response');
+
+            return callback(null, payload);
+        }
+    }
+}
---
+++ tests/index.js
@@ -0,0 +1,242 @@
+﻿var test = require('tape'),
+    express = require('express'),
+    request = require('supertest'),
+    icylog = require('../index');
+
+test('Should load default options', function (t) {
+    var app = createApp(new Error('Some error'));
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(500)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.true(res.body.error, 'should exist');
+            t.end();
+        });
+});
+
+test('Should respect specified options', function (t) {
+    var app = createApp(new Error('Some error'), {
+        env: 'test',
+        logger: console
+    });
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(500)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.false(res.body.error, 'should not exist');
+            t.end();
+        });
+});
+
+test('Should ensure error object', function (t) {
+    var error = 'Not an error object',
+        app = createApp(error);
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(500)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.equal(typeof res.body.error, 'object', 'should match');
+            t.equal(res.body.message, error, 'should match');
+            t.end();
+        });
+});
+
+test('Should set default status code', function (t) {
+    var app = createApp(new Error('Some error'));
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(500)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.equal(res.status, 500, 'should match');
+            t.end();
+        });
+});
+
+test('Should retain existing status', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
+
+    error.status = 401;
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(error.status)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.equal(res.status, error.status, 'should match');
+            t.end();
+        });
+});
+
+test('Should retain existing statusCode', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
+
+    error.statusCode = 401;
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(error.statusCode)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.equal(res.status, error.statusCode, 'should match');
+            t.end();
+        });
+});
+
+test('Should refuse to invoke httpHandler', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
+
+    error.status = 200;
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(500)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.equal(res.body.message.substring(0, 23), "An unexpected exception", 'should match');
+            t.end();
+        });
+});
+
+test('Should create payload', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
+
+    error.code = 404;
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(error.code)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.false(res.body.success, 'should be false');
+            t.equal(res.body.message, error.message, 'should match');
+            t.end();
+        });
+});
+
+test('Should return plain text via JSON.stringify', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
+
+    error.status = 404;
+
+    request(app)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect('Content-Type', /text\/plain/)
+        .expect(error.status)
+        .end(function (err, res) {
+            var json = JSON.parse(res.text),
+                errorObj = JSON.parse(JSON.stringify(error));
+
+            t.error(err, 'should not exist');
+            t.equal(typeof res.text, 'string', 'should match');
+            t.false(json.success, 'should be false');
+            t.equal(json.message, error.message, 'should match');
+            t.deepEqual(json.error, errorObj, 'should match');
+            t.end();
+        });
+});
+
+test('Should return "Not Acceptable"', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
+
+    error.status = 404;
+
+    request(app)
+        .get('/')
+        .set('Accept', 'x-unknown')
+        .expect('Content-Type', /text\/plain/)
+        .expect(406)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.equal(typeof res.text, 'string', 'should match');
+            t.equal(res.text, 'Not Acceptable', 'should match');
+            t.end();
+        });
+});
+
+test('Should set nosniff header', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
+
+    request(app)
+        .get('/')
+        .expect('X-Content-Type-Options', 'nosniff')
+        .expect(500)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.end();
+        });
+});
+
+test('Should invoke wsHandler', function (t) {
+    var error = new Error('Some error'),
+        errorHandler = icylog().ws;
+
+    errorHandler(error, function (err, payload) {
+        t.error(err, 'should not exist');
+        t.false(payload.success, 'should be false');
+        t.equal(payload.message, error.message, 'should match');
+        t.equal(payload.error, error, 'should match');
+        t.equal(payload.error.status, 500, 'should match');
+        t.end();
+    });
+});
+
+test('Should refuse to invoke wsHandler', function (t) {
+    var error = new Error('Some error'),
+        errorHandler = icylog().ws;
+
+    error.status = 200;
+
+    errorHandler(error, function (err, payload) {
+        t.equal(err.message.substring(0, 23), "An unexpected exception", 'should match');
+        t.end();
+    });
+});
+
+function createApp(error, options) {
+    var app = express();
+
+    app.get('*', function (req, res, next) {
+        next(error);
+    });
+
+    app.use(icylog(options).http);
+
+    app.use(function (err, req, res, next) {
+        res.status(500);
+        res.send(err);
+    });
+
+    return app;
+}
