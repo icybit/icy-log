c60e41255d3618aa47c7b6a5f2036e8030b76d9c
Giedrius Sirevicius <giedriussir@gmail.com>
2016-08-08 14:37:30 +0300

    Updated Error object assurance and propagation logic. Added CustomError type exposure

--- tests/index.js
+++ tests/index.js
@@ -3,10 +3,11 @@
 var test = require('tape'),
     express = require('express'),
     request = require('supertest'),
-    icylog = require('../index');
+    icylog = require('../index'),
+    CustomError = icylog().CustomError();
 
 test('Should load default options', function (t) {
-    var app = createApp(new Error('Some error'));
+    var app = createApp(new CustomError('Some error', 500));
 
     request(app)
         .get('/')
@@ -21,7 +22,7 @@ test('Should load default options', function (t) {
 });
 
 test('Should respect specified options', function (t) {
-    var app = createApp(new Error('Some error'), {
+    var app = createApp(new CustomError('Some error', 500), {
         env: 'test',
         logger: console
     });
@@ -38,10 +39,9 @@ test('Should respect specified options', function (t) {
         });
 });
 
-test('Should set specified error name', function (t) {
-    var app = createApp(new Error('Some error'), {
-        errName: 'SomeError'
-    });
+test('Should propagate if no status code has been set', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
 
     request(app)
         .get('/')
@@ -50,7 +50,7 @@ test('Should set specified error name', function (t) {
         .expect(500)
         .end(function (err, res) {
             t.error(err, 'should not exist');
-            t.equal(res.body.error.name, 'SomeError', 'should match');
+            t.deepEqual(res.body, error, 'should match');
             t.end();
         });
 });
@@ -66,14 +66,35 @@ test('Should convert string to ExecutionError object', function (t) {
         .expect(500)
         .end(function (err, res) {
             t.error(err, 'should not exist');
-            t.equal(typeof res.body.error, 'object', 'should match');
+            t.true(res.body.error, 'should exist');
+            t.equal(res.body.error.substring(0, 14), 'ExecutionError', 'should match');
             t.equal(res.body.message, error, 'should match');
             t.end();
         });
 });
 
-test('Should convert object to ExecutionError object', function (t) {
-    var error = { message: 'Some error', status: 500 },
+test('Should retain Error object', function (t) {
+    var error = new Error('Some error'),
+        app = createApp(error);
+
+    error.status = 404;
+
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(error.status)
+        .end(function (err, res) {
+            t.error(err, 'should not exist');
+            t.true(res.body.error, 'should exist');
+            t.equal(res.body.error.substring(0, 5), error.name, 'should match');
+            t.equal(res.body.message, error.message, 'should match');
+            t.end();
+        });
+});
+
+test('Should retain CustomError type object', function (t) {
+    var error = new icylog().CustomError('SomeError')('Some error', 404),
         app = createApp(error);
 
     request(app)
@@ -83,67 +104,16 @@ test('Should convert object to ExecutionError object', function (t) {
         .expect(error.status)
         .end(function (err, res) {
             t.error(err, 'should not exist');
-            t.equal(typeof res.body.error, 'object', 'should match');
+            t.true(res.body.error, 'should exist');
+            t.equal(res.body.error.substring(0, 9), error.name, 'should match');
             t.equal(res.body.message, error.message, 'should match');
             t.end();
         });
 });
 
-test('Should convert Error to ExecutionError object', function (t) {
-    var error = new Error('Some error'),
+test('Should retain existing status code', function (t) {
+    var error = new CustomError('Some error', 401),
         app = createApp(error);
-
-    request(app)
-        .get('/')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /application\/json/)
-        .expect(500)
-        .end(function (err, res) {
-            t.error(err, 'should not exist');
-            t.equal(typeof res.body.error, 'object', 'should match');
-            t.equal(res.body.message, error.message, 'should match');
-            t.end();
-        });
-});
-
-test('Should retain ExecutionError object', function (t) {
-    var error = new icylog().ExecutionError('Some error'),
-        app = createApp(error);
-
-    request(app)
-        .get('/')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /application\/json/)
-        .expect(500)
-        .end(function (err, res) {
-            t.error(err, 'should not exist');
-            t.equal(typeof res.body.error, 'object', 'should match');
-            t.deepEqual(res.body.error, error, 'should match');
-            t.equal(res.body.message, error.message, 'should match');
-            t.end();
-        });
-});
-
-test('Should set default status code', function (t) {
-    var app = createApp(new Error('Some error'));
-
-    request(app)
-        .get('/')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /application\/json/)
-        .expect(500)
-        .end(function (err, res) {
-            t.error(err, 'should not exist');
-            t.equal(res.status, 500, 'should match');
-            t.end();
-        });
-});
-
-test('Should retain existing status', function (t) {
-    var error = new Error('Some error'),
-        app = createApp(error);
-
-    error.status = 401;
 
     request(app)
         .get('/')
@@ -157,29 +127,9 @@ test('Should retain existing status', function (t) {
         });
 });
 
-test('Should retain existing statusCode', function (t) {
-    var error = new Error('Some error'),
+test('Should propagate if non-error status code has been set', function (t) {
+    var error = new CustomError('Some error', 200),
         app = createApp(error);
-
-    error.statusCode = 401;
-
-    request(app)
-        .get('/')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /application\/json/)
-        .expect(error.statusCode)
-        .end(function (err, res) {
-            t.error(err, 'should not exist');
-            t.equal(res.status, error.statusCode, 'should match');
-            t.end();
-        });
-});
-
-test('Should refuse to invoke httpHandler', function (t) {
-    var error = new Error('Some error'),
-        app = createApp(error);
-
-    error.status = 200;
 
     request(app)
         .get('/')
@@ -188,16 +138,14 @@ test('Should refuse to invoke httpHandler', function (t) {
         .expect(500)
         .end(function (err, res) {
             t.error(err, 'should not exist');
-            t.equal(res.body.message.substring(0, 23), 'An unexpected exception', 'should match');
+            t.deepEqual(res.body, error, 'should match');
             t.end();
         });
 });
 
 test('Should create payload', function (t) {
-    var error = new Error('Some error'),
+    var error = new CustomError('Some error', 404),
         app = createApp(error);
-
-    error.status = 404;
 
     request(app)
         .get('/')
@@ -208,16 +156,14 @@ test('Should create payload', function (t) {
             t.error(err, 'should not exist');
             t.false(res.body.success, 'should be false');
             t.equal(res.body.message, error.message, 'should match');
+            t.equal(res.body.error, error.stack, 'should match');
             t.end();
         });
 });
 
 test('Should return plain text via JSON.stringify', function (t) {
-    var error = new Error('Some error'),
-        executionError = new icylog().ExecutionError(error, 404),
+    var error = new CustomError('Some error', 404),
         app = createApp(error);
-
-    error.status = 404;
 
     request(app)
         .get('/')
@@ -228,19 +174,16 @@ test('Should return plain text via JSON.stringify', function (t) {
             var json = JSON.parse(res.text);
 
             t.error(err, 'should not exist');
-            t.equal(typeof res.text, 'string', 'should match');
             t.false(json.success, 'should be false');
             t.equal(json.message, error.message, 'should match');
-            t.deepEqual(json.error, executionError, 'should match');
+            t.equal(json.error, error.stack, 'should match');
             t.end();
         });
 });
 
 test('Should return "Not Acceptable"', function (t) {
-    var error = new Error('Some error'),
+    var error = new CustomError('Some error', 404),
         app = createApp(error);
-
-    error.status = 404;
 
     request(app)
         .get('/')
@@ -249,20 +192,19 @@ test('Should return "Not Acceptable"', function (t) {
         .expect(406)
         .end(function (err, res) {
             t.error(err, 'should not exist');
-            t.equal(typeof res.text, 'string', 'should match');
             t.equal(res.text, 'Not Acceptable', 'should match');
             t.end();
         });
 });
 
 test('Should set nosniff header', function (t) {
-    var error = new Error('Some error'),
+    var error = new CustomError('Some error', 400),
         app = createApp(error);
 
     request(app)
         .get('/')
         .expect('X-Content-Type-Options', 'nosniff')
-        .expect(500)
+        .expect(error.status)
         .end(function (err, res) {
             t.error(err, 'should not exist');
             t.end();
@@ -271,26 +213,24 @@ test('Should set nosniff header', function (t) {
 
 test('Should invoke wsHandler', function (t) {
     var handler = icylog(),
-        error = new Error('Some error'),
-        executionError = new handler.ExecutionError(error);
+        error = new CustomError('Some error', 400);
 
     handler.ws(error, function (err, payload) {
         t.error(err, 'should not exist');
         t.false(payload.success, 'should be false');
         t.equal(payload.message, error.message, 'should match');
-        t.deepEqual(payload.error, executionError, 'should match');
+        t.equal(payload.error, error.stack, 'should match');
         t.end();
     });
 });
 
-test('Should refuse to invoke wsHandler', function (t) {
-    var error = new Error('Some error'),
+test('Should propagate from wsHandler if non-error status code has been set', function (t) {
+    var error = new CustomError('Some error', 200),
         errorHandler = icylog().ws;
 
-    error.status = 200;
-
     errorHandler(error, function (err, payload) {
-        t.equal(err.message.substring(0, 23), 'An unexpected exception', 'should match');
+        t.equal(err.message, error.message, 'should match');
+        t.equal(err.stack, error.stack, 'should match');
         t.end();
     });
 });
